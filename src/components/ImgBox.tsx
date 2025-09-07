0e5dc9b8b26570d982d643603ba9bee4d02a8caf
adhyan-jain <adhyanjain2006@gmail.com>
2025-09-07 20:31:24 +0530

    feat: added lamp light

--- src/components/ImgBox.tsx
+++ src/components/ImgBox.tsx
@@ -15,7 +15,7 @@ export default function ImgBox({ text, img, title, selected = false, onClick }: 
       className={`h-[250px] flex-1 min-w-0 border-2 rounded-4xl m-2 border-emerald-500 flex flex-col justify-around col-span-2 row-span-2 cursor-pointer transition-colors duration-200 ${selected ? 'bg-gray-700' : 'bg-transparent'}`}
       onClick={onClick}
     >
-      <h3 className="text-[22px] font-bold text-center mb-2">{text}</h3>
+      <h3 className="text-[22px] font-bold text-center my-2">{text}</h3>
       <Image
         src={img}
         alt={title}
