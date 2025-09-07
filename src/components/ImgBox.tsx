3d1f8681edd20a3b0ee5928029fd9402ad581d96
Ridwick0210 <ridwick2lingan@gmail.com>
2025-09-08 00:10:02 +0530

    pls leave me alone

--- src/components/ImgBox.tsx
+++ src/components/ImgBox.tsx
@@ -1,4 +1,3 @@
-import { div } from "motion/react-client";
 import Image from "next/image";
 
 type Props = {
@@ -12,17 +11,18 @@ type Props = {
 export default function ImgBox({ text, img, title, selected = false, onClick }: Props) {
   return (
     <div
-      className={`h-[250px] flex-1 min-w-0 border-2 rounded-4xl m-2 border-emerald-500 flex flex-col justify-around col-span-2 row-span-2 cursor-pointer transition-colors duration-200 ${selected ? 'bg-gray-700' : 'bg-transparent'}`}
+      className={`flex-1 h-[120px] sm:h-[180px] md:h-[250px] 00 min-w-0 border-2 rounded-3xl m-1 sm:m-2 border-emerald-500 flex flex-col justify-around col-span-2 row-span-2 cursor-pointer transition-colors duration-200 ${selected ? "bg-gray-700" : "bg-transparent"}`}
       onClick={onClick}
     >
-      <h3 className="text-[22px] font-bold text-center my-2">{text}</h3>
+      <h3 className="text-center font-bold my-2 text-[10px] sm:text-sm md:text-lg">
+        {text}
+      </h3>
       <Image
         src={img}
         alt={title}
         width={150}
         height={150}
-        className="mx-auto"
-      />
+        className="mx-auto h-[40px] sm:h-[60px] md:h-[100px] w-auto aspect-1/1" />
     </div>
   );
 }
